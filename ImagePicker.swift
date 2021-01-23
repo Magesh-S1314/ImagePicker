3fc68b00128856c9a03593a8d49129096b7ad52b
Magesh <magesh.s@doodleblue.com>
2021-01-23 13:23:18 +0530

    20210123
    Initial commit

---
+++ ImagePicker.swift
@@ -0,0 +1,146 @@
+//
+//  ImagePicker.swift
+//
+//  Created by magesh on 12/01/21.
+//
+
+import Foundation
+import UIKit
+import MobileCoreServices
+import AVFoundation
+import Photos
+
+public protocol ImagePickerDelegate: class {
+    func didSelect(image: UIImage?)
+}
+
+enum AttachmentType: String{
+    case camera, photoLibrary
+}
+
+open class ImagePicker: NSObject {
+    
+    private let pickerController: UIImagePickerController
+    private weak var presentationController: UIViewController?
+    private weak var delegate: ImagePickerDelegate?
+    
+    public init(presentationController: UIViewController, delegate: ImagePickerDelegate) {
+        self.pickerController = UIImagePickerController()
+        
+        super.init()
+        
+        self.presentationController = presentationController
+        self.delegate = delegate
+        
+        self.pickerController.delegate = self
+        self.pickerController.allowsEditing = true
+        self.pickerController.mediaTypes = ["public.image"]
+    }
+    
+    private func action(for type: UIImagePickerController.SourceType, title: String) -> UIAlertAction? {
+        guard UIImagePickerController.isSourceTypeAvailable(type) else {
+            return nil
+        }
+        
+        return UIAlertAction(title: title, style: .default) { [unowned self] _ in
+            self.authorisationStatus(for: type)
+        }
+    }
+    
+    public func present(from sourceView: UIView) {
+        
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        if let action = self.action(for: .camera, title: "Take photo") {
+            alertController.addAction(action)
+        }
+        if let action = self.action(for: .savedPhotosAlbum, title: "Camera roll") {
+            alertController.addAction(action)
+        }
+        if let action = self.action(for: .photoLibrary, title: "Photo library") {
+            alertController.addAction(action)
+        }
+        
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            alertController.popoverPresentationController?.sourceView = sourceView
+            alertController.popoverPresentationController?.sourceRect = sourceView.bounds
+            alertController.popoverPresentationController?.permittedArrowDirections = [.down, .up]
+        }
+        
+        self.presentationController?.present(alertController, animated: true)
+    }
+    
+    private func pickerController(_ controller: UIImagePickerController, didSelect image: UIImage?) {
+        controller.dismiss(animated: true, completion: nil)
+        
+        self.delegate?.didSelect(image: image)
+    }
+    
+    func presentPicker(){
+        DispatchQueue.main.async {
+            self.presentationController?.present(self.pickerController, animated: true)
+        }
+    }
+    
+    func authorisationStatus(for type: UIImagePickerController.SourceType){
+        
+        self.pickerController.sourceType = type
+        
+        let cameraAuthorizationStatus = AVCaptureDevice.authorizationStatus(for: .video)
+        
+        switch cameraAuthorizationStatus {
+        case .notDetermined: requestCameraPermission()
+        case .authorized: presentPicker()
+        case .restricted, .denied: alertCameraAccessNeeded()
+        @unknown default:
+            break
+        }
+    }
+    
+    func requestCameraPermission() {
+        AVCaptureDevice.requestAccess(for: .video, completionHandler: {accessGranted in
+            guard accessGranted == true else { return }
+            self.presentPicker()
+        })
+    }
+    
+    func alertCameraAccessNeeded() {
+        let settingsAppURL = URL(string: UIApplication.openSettingsURLString)!
+        
+        let alert = UIAlertController(
+            title: "Need Camera Access",
+            message: "Camera access is required to make full use of this app.",
+            preferredStyle: UIAlertController.Style.alert
+        )
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+        alert.addAction(UIAlertAction(title: "Allow Camera", style: .cancel, handler: { (alert) -> Void in
+            UIApplication.shared.open(settingsAppURL, options: [:], completionHandler: nil)
+        }))
+        
+        DispatchQueue.main.async {
+            self.presentationController?.present(alert, animated: true)
+        }
+    }
+}
+
+extension ImagePicker: UIImagePickerControllerDelegate {
+    
+    public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.pickerController(picker, didSelect: nil)
+    }
+    
+    public func imagePickerController(_ picker: UIImagePickerController,
+                                      didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        guard let image = info[.editedImage] as? UIImage else {
+            return self.pickerController(picker, didSelect: nil)
+        }
+        self.pickerController(picker, didSelect: image)
+    }
+}
+
+extension ImagePicker: UINavigationControllerDelegate {
+    
+}
